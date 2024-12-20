53d43fbdbc18ec9b5d83e524774a6258524b9b4c
mohan <kmohan0910@gmail.com>
2024-12-21 00:23:50 +0530

    Updated UI & Edge Cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,127 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  passwordLength: number = 8;
-  includeAlphabets: boolean = true;
-  includeNumbers: boolean = false;
-  includeSpecialCharacters: boolean = false;
+  tooltipText: string = 'Copy to clipboard';
+  passwordForm: FormGroup;
+  generatedPassword = '';
+  readonly alphabetChars =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  readonly numberChars = '0123456789';
+  readonly specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
 
-  password: string = '';
+  constructor(private fb: FormBuilder) {
+    this.passwordForm = this.fb.group({
+      length: [
+        8,
+        [Validators.required, Validators.min(1), Validators.max(128)],
+      ],
+      includeAlphabets: [true],
+      includeNumbers: [false],
+      includeSpecialChars: [false],
+      noRepeats: [false],
+    });
+    this.passwordForm.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+  updateTooltip() {
+    this.tooltipText = 'Copy to clipboard';
+  }
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.generatedPassword);
+    this.tooltipText = 'Copied!';
+  }
 
   generatePassword() {
-    const alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numbers = '0123456789';
-    const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
-
-    let charPool = '';
-    if (this.includeAlphabets) charPool += alphabets;
-    if (this.includeNumbers) charPool += numbers;
-    if (this.includeSpecialCharacters) charPool += specialChars;
-
-    if (!charPool) {
-      this.password = 'Please select at least one option.';
+    if (this.passwordForm.invalid) {
+      alert('Please fix the errors in the form before generating a password.');
       return;
     }
 
-    this.password = Array.from(
-      { length: this.passwordLength },
-      () => charPool[Math.floor(Math.random() * charPool.length)]
-    ).join('');
+    const {
+      length,
+      includeAlphabets,
+      includeNumbers,
+      includeSpecialChars,
+      noRepeats,
+    } = this.passwordForm.value;
+    let flagCount = 0;
+    let charPool = '';
+    if (includeAlphabets) {
+      charPool += this.alphabetChars;
+      flagCount++;
+    }
+    if (includeNumbers) {
+      charPool += this.numberChars;
+      flagCount++;
+    }
+    if (includeSpecialChars) {
+      charPool += this.specialChars;
+      flagCount++;
+    }
+    const selectedFlags = [
+      includeAlphabets,
+      includeNumbers,
+      includeSpecialChars,
+    ].filter(Boolean).length;
+    if (length < selectedFlags) {
+      alert(
+        'Password length must be at least equal to the number of selected character types.'
+      );
+      return;
+    }
+
+    if (!charPool) {
+      alert(
+        'Please select at least one character type to generate a password.'
+      );
+      return;
+    }
+
+    if (noRepeats && length > charPool.length) {
+      alert(
+        'Password length exceeds the number of unique characters available. Disable "No Repeats" or reduce the length.'
+      );
+      return;
+    }
+
+    this.generatedPassword = this.generateRandomPassword(
+      charPool,
+      length,
+      noRepeats
+    );
+  }
+
+  private generateRandomPassword(
+    pool: string,
+    length: number,
+    noRepeats: boolean
+  ): string {
+    let result = '';
+    const usedChars = new Set<string>();
+
+    for (let i = 0; i < length; i++) {
+      let randomChar;
+
+      do {
+        const randomIndex = Math.floor(Math.random() * pool.length);
+        randomChar = pool[randomIndex];
+      } while (noRepeats && usedChars.has(randomChar));
+
+      result += randomChar;
+      if (noRepeats) usedChars.add(randomChar);
+    }
+
+    return result;
   }
 }
